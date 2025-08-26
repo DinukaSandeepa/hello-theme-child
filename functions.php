bc8c4402264cc6725882fd717c79c9144cace089
Dinuka Sandeepa <dinukasandeep50@gmail.com>
2025-08-26 22:15:43 +0530

    Add tour itinerary shortcode functionality

--- functions.php
+++ functions.php
@@ -34,3 +34,26 @@ function hello_elementor_child_scripts_styles() {
 
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+function tour_itinerary_shortcode() {
+    ob_start();
+    $itinerary = SCF::get('itinerary');
+    if ($itinerary) {
+        echo '<div class="tour-itinerary">';
+        foreach ($itinerary as $index => $day) {
+            echo '<div class="tour-day">';
+            echo '<h3>Day ' . ($index + 1) . ': ' . esc_html($day['day_title']) . '</h3>';
+            echo '<p>' . esc_html($day['activities']) . '</p>';
+            if (!empty($day['meals'])) {
+                echo '<p><strong>Meals:</strong> ' . esc_html($day['meals']) . '</p>';
+            }
+            if (!empty($day['hotel'])) {
+                echo '<p><strong>Hotel:</strong> ' . esc_html($day['hotel']) . '</p>';
+            }
+            echo '</div>';
+        }
+        echo '</div>';
+    }
+    return ob_get_clean();
+}
+add_shortcode('tour_itinerary', 'tour_itinerary_shortcode');
